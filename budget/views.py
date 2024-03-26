96123198aee5bd8354db6ac0661e217efe3deff1
p_bhandari <147399832+bhandari-p@users.noreply.github.com>
2024-03-26 20:34:28 +0545

    a

--- budget/views.py
+++ budget/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def project_list(request):
-    return render(request,'budget/project-li.html')
+    return render(request,'budget/project-list.html')
 
 def project_detail(request,project_slug):
     return render(request,'budget/project-detail.html')
